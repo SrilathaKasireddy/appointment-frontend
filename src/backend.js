166d51ad912354c642299717e455bf56e53445d6
SrilathaKasireddy <kasireddysrilatha17@gmail.com>
2023-01-11 19:42:26 +0530

    deploy changes

--- src/backend.js
+++ src/backend.js
@@ -1,5 +1,5 @@
 // export const API = process.env.REACT_APP_BACKEND;
-export const API = "http://localhost:3001/api";
+export const API = "https://appointment-bkend.onrender.com/api";
 
 export const cPrint = (params) => {
   console.log(params);
